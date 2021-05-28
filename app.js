c0db86a9584322c85b93d8b36a3285df8cee86f6
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-05-27 22:17:55 -0300

    usando Promise.all

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const fetchPokemon = () => {
     const pokemonPromises = []
 
     for (const i = 1; i <= 150; i++) { //  cada loop, a promise seja adicionada em um array de promise
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(res => res.json)) // a cada iteração adicionar um item no final do array.
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(res => res.json())) // a cada iteração adicionar um item no final do array.
             // requisição http(AJAX)
             // fetch retorna uma promise, usar o then
     }
 
+    Promise.all(pokemonPromises) // Quando todas as promises estiveram resolvidas, retorna uma promise.
+        .then(pokemons => {
+            console.log(pokemons)
+        })
 }
 
 fetchPokemon() // invocando a função

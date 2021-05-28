30c718813f1c4c76d3b6bfb125d4f9d07a132b0e
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-05-27 22:11:32 -0300

    criando um for para cada iteração executar o fetch

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //Função que vai buscar os Pokemons
 const fetchPokemon = () => {
     const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}` // id para representar todos os pokemons
-    fetch(url) // requisição http(AJAX)
-        .then(res => res.json) // fetch retorna uma promise, usar o then
-        .then(pokemon => { // o then retorna uma promise também.
-            console.log(pokemon)
-        })
+
+    for (const i = 1; i <= 150; i++) {
+        fetch(getPokemonUrl) // requisição http(AJAX)
+            .then(res => res.json) // fetch retorna uma promise, usar o then
+    }
+
 }
 
 fetchPokemon() // invocando a função

c567853a0d7aa70f805fba0730574061544efab7
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-05-27 22:59:16 -0300

    identando código

--- app.js
+++ app.js
@@ -3,38 +3,39 @@ const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}` // id para
 const generatePokemonPromisess = () => Array(150).fill().map((_, index) =>
         fetch(getPokemonUrl(index + 1)).then(res => res.json())) // gerar e retornar um array de promises pokemons.
 
-const fetchPokemon = () => {
-    const pokemonPromises = generatePokemonPromisess() // Usar o for para preencher o array vazio.
-
-    // for (let i = 1; i <= 150; i++) { //  cada loop, a promise seja adicionada em um array de promise
-    //     pokemonPromises.push(fetch(getPokemonUrl(i)).then(res => res.json())) // a cada iteração adicionar um item no final do array.
-    //         // requisição http(AJAX)
-    //         // fetch retorna uma promise, usar o then
-    // }
-
-    Promise.all(pokemonPromises) // Quando todas as promises estiveram resolvidas, retorna uma promise.
-        .then(pokemons => {
+const generateHTML = pokemons => {
 
 
-            return pokemons.reduce((acum, pokemon) => { // reduzir o array em uma string.
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name) // variável que vai guardar os tipos dos pokemons.
+    return pokemons.reduce((acum, pokemon) => { // reduzir o array em uma string.
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name) // variável que vai guardar os tipos dos pokemons.
 
-                acum += ` 
-                <li class="card ${types[0]}" >
-                <img class= " card-image" alt="${pokemon.name}" src= "https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
-               <h2 class = "card-title ">${pokemon.id} ${pokemon.name}</h2>
-               <p class="card-subtitle">${types.join(' | ')}</p>
-                </li>`
-                return acum
-            }, '')
+        acum += ` 
+        <li class="card ${types[0]}" >
+        <img class= " card-image" alt="${pokemon.name}" src= "https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
+       <h2 class = "card-title ">${pokemon.id} ${pokemon.name}</h2>
+       <p class="card-subtitle">${types.join(' | ')}</p>
+        </li>`
+        return acum
+    }, '')
 
 
-        })
-        .then(pokemons => { // conter uma função e inserir a const ul e as li nas páginas
-            const ul = document.querySelector('[data-js="pokedex"]') // Inserindo LI na página
-            ul.innerHTML = pokemons // inserindo li dentro da ul.
-
-        })
 }
 
-fetchPokemon() // invocando a função
+const insertPokemonsIntoPage = pokemons => { // conter uma função e inserir a const ul e as li nas páginas
+    const ul = document.querySelector('[data-js="pokedex"]') // Inserindo LI na página
+    ul.innerHTML = pokemons // inserindo li dentro da ul.
+
+}
+
+
+const pokemonPromises = generatePokemonPromisess() // Usar o for para preencher o array vazio.
+
+// for (let i = 1; i <= 150; i++) { //  cada loop, a promise seja adicionada em um array de promise
+//     pokemonPromises.push(fetch(getPokemonUrl(i)).then(res => res.json())) // a cada iteração adicionar um item no final do array.
+//         // requisição http(AJAX)
+//         // fetch retorna uma promise, usar o then
+// }
+
+Promise.all(pokemonPromises) // Quando todas as promises estiveram resolvidas, retorna uma promise.
+    .then(generateHTML)
+    .then(insertPokemonsIntoPage)

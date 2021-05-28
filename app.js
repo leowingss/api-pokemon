73900485664e80eae756724a1dcda6b73cb48b2c
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-05-27 22:08:57 -0300

    tornando a url dinâmica

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Função que vai buscar os Pokemons
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/` // url da api Pokemon
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}` // id para representar todos os pokemons
     fetch(url) // requisição http(AJAX)
         .then(res => res.json) // fetch retorna uma promise, usar o then
         .then(pokemon => { // o then retorna uma promise também.

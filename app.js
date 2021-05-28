9a266b8ba5766ffc96a14ab6584d3338b81e915b
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-05-27 22:07:58 -0300

    encadeando then no fetch

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //Função que vai buscar os Pokemons
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/`
+    const url = `https://pokeapi.co/api/v2/pokemon/` // url da api Pokemon
     fetch(url) // requisição http(AJAX)
-
+        .then(res => res.json) // fetch retorna uma promise, usar o then
+        .then(pokemon => { // o then retorna uma promise também.
+            console.log(pokemon)
+        })
 }
 
 fetchPokemon() // invocando a função
